bdfcc6b69a8b870b1b8fff031a62af8ecf19e618
sagarmaiti26 <sagarmaiti277@gmail.com>
2024-06-23 15:09:16 +0530

    Clerk Error

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import MobileNav from "../../components/shared/MobileNav";
+import Sidebar from "../../components/shared/Sidebar";
+import { Toaster } from "../../components/ui/toaster";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      {/* sidebar component */}
+      <Sidebar />
+
+      {/* mobile nav */}
+      <MobileNav />
+
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+
+      <Toaster />
+    </main>
+  );
+};
+
+export default Layout;
