56245c890cbddee1e1866fd357940eba39bed528
sagarmaiti26 <sagarmaiti277@gmail.com>
2024-06-30 11:22:40 +0530

    Clerk New Update

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { withClerkMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook/clerk", "/api/webhook/stripe"],
-  ignoredRoutes:["/api/webhook/clerk"]
+export default withClerkMiddleware({
+ 
 });
 
 export const config = {
-  matcher: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
 };
